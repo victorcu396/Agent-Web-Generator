ea82be8d7e2ad842bb9a587e71997efae92939b1
victorcu396 <vmenendezmata@gmail.com>
2026-02-11 12:27:13 +0100

    Initial project setup

---
+++ backend/src/pyproyect_toml/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2026-present victorcu396 <vmenendezmata@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ backend/tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2026-present victorcu396 <vmenendezmata@gmail.com>
+#
+# SPDX-License-Identifier: MIT
